0808ff3feebea0bcb394df5cb176737b99587873
Vitaliy Inshakov <michael8913@gmail.com>
2021-02-13 23:40:24 +0200

    routes 404

---
+++ src/pages/client/restaurants.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Restaurants = () =>(<h1>Restaurants</h1>);
+
+export default Restaurants;
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import { meQuery } from "../__generated__/meQuery";
+import Restaurants from "../pages/client/restaurants";
+
+const ClientRoutes = () => <>
+    <Route path="/" exact><Restaurants /></Route>
+</>;
 
 const ME_QUERY = gql`
     query meQuery {
@@ -24,8 +30,11 @@ export const LoggedInRouter = () => {
     }
 
     return (
-        <div>
-            <h1>{data.me.role}</h1>
-        </div>
+        <Router>
+            <Switch>
+                {data.me.role === "Client" && <ClientRoutes />}
+                <Redirect to="/" />
+            </Switch>
+        </Router>
     );
 };
